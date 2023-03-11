72173250453f9aed952f1ac0fdfc14ad5611742f
Bennette Benoy <benb@Bennettes-Laptop.local>
2023-03-11 22:47:54 +1000

    configure formatting and fix indenting

--- lua/core/post_load.lua
+++ lua/core/post_load.lua
@@ -2,14 +2,14 @@ local keymaps = require "core.keymaps"
 local registry = require "mason-registry"
 
 registry:on(
-    "package:install:success",
-    vim.schedule_wrap(function(pkg, handle)
-        print(string.format("Successfully installed %s", pkg.name))
-        vim.defer_fn(function()
-          vim.api.nvim_command('LspStart')
-          print("LSP Started")
-        end, 1000)
-    end
+  "package:install:success",
+  vim.schedule_wrap(function(pkg, handle)
+    print(string.format("Successfully installed %s", pkg.name))
+    vim.defer_fn(function()
+      vim.api.nvim_command('LspStart')
+      print("LSP Started")
+    end, 1000)
+  end
   )
 )
 
@@ -31,7 +31,7 @@ vim.g.mapleader = keymaps.leader
 map('i', keymaps.escape, '<Esc>')
 
 -- Clear search highlighting
-map('n', keymaps.clear_search_highlighting , ':nohl<CR>')
+map('n', keymaps.clear_search_highlighting, ':nohl<CR>')
 
 -- Move around splits using Ctrl + {h,j,k,l}
 map('n', keymaps.move_to_left_window, '<C-w>h')
@@ -42,27 +42,30 @@ map('n', keymaps.move_to_right_window, '<C-w>l')
 -- Save file
 map('n', keymaps.save_file, ':w<CR>')
 
+-- Format file
+map('n', keymaps.lsp_format, ':LspZeroFormat<CR>')
+
 -- Nvim Tree
 map('n', keymaps.open_explorer, ':NvimTreeToggle<CR>') -- Toggle Open/Close
 
 -- Bufferline
 map('n', keymaps.next_buffer, ':BufferLineCycleNext<CR>')
-map('n',keymaps.previous_buffer, ':BufferLineCyclePrev<CR>')
-map('n',keymaps.close_buffer, ':bdelete<CR>')
+map('n', keymaps.previous_buffer, ':BufferLineCyclePrev<CR>')
+map('n', keymaps.close_buffer, ':bdelete<CR>')
 
 -- Telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n',keymaps.telescope_find_files, builtin.find_files, {})
-vim.keymap.set('n',keymaps.telescope_live_grep, builtin.live_grep, {})
-vim.keymap.set('n',keymaps.telescope_buffers, builtin.buffers, {})
-vim.keymap.set('n',keymaps.telescope_help_tags, builtin.help_tags, {})
+vim.keymap.set('n', keymaps.telescope_find_files, builtin.find_files, {})
+vim.keymap.set('n', keymaps.telescope_live_grep, builtin.live_grep, {})
+vim.keymap.set('n', keymaps.telescope_buffers, builtin.buffers, {})
+vim.keymap.set('n', keymaps.telescope_help_tags, builtin.help_tags, {})
 
 -- Trouble
 map("n", keymaps.trouble_toggle, ':TroubleToggle<CR>')
 map("n", keymaps.trouble_quick_fix, ':TroubleToggle quickfix<CR>')
 
 -- ToggleTerm
-vim.cmd [[ 
+vim.cmd [[
   autocmd TermEnter term://*toggleterm#*
         \ tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>
 
